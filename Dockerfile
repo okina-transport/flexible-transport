973050bcfe0e85d84e83da82c872a250b32cff63
Mathieu Hicauber <mhicauber@okina.fr>
2020-11-02 17:43:27 +0100

    Builds script OK.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,26 @@
+# Image de build UI
+#TODO : migrer vers un node plus récent
+FROM node:9.11.2-alpine as build
+
+WORKDIR /app
+
+# copy everything to current working directory
+COPY . ./
+
+RUN npm install &&\
+    npm run build
+
+# Construction de l'image de l'appli
 FROM node:9.11.2-alpine
 
-# Server
+## Server
 ENV CONTENT_BASE=/app/ui
 COPY server /app/server
 WORKDIR /app/server
 RUN npm install --production
 
-# Application
-COPY build /app/ui/
+# UI
+COPY --from=build /app/build /app/ui/
 ARG BUILD_DATE
 ENV BUILD_DATE=$BUILD_DATE
 
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# Lancer la construction de l'image docker.
+# Passe en argument la clé publique de la machine hote (qui doit du coup avoir github dans ses "known_hosts" pour permettre le clonage des repos nécessaires depuis github depuis le container
+docker build --no-cache --tag=registry.okina.fr/mosaic/enki:${PACKAGE_VERSION} --force-rm=true . --build-arg SSH_KEY="$(cat ~/.ssh/id_rsa)"
+docker push registry.okina.fr/mosaic/enki:${PACKAGE_VERSION}
