2bfe0d9f5ac2f09a642fd3fe920ec04ef27aff2c
Mathieu Hicauber <mhicauber@okina.fr>
2021-01-14 12:52:09 +0100

    fix: added custom roles attribute in keycloak object

--- src/custom.typings.d.ts
+++ src/custom.typings.d.ts
@@ -3,5 +3,14 @@ import 'date-fns/formatDuration';
 // Awaiting release of https://github.com/date-fns/date-fns/pull/1881
 declare module 'date-fns/formatDuration' {
   function formatDuration(duration: Duration, Options?: any): string;
+
   export = formatDuration;
+}
+
+declare global {
+  namespace Keycloak {
+    interface KeycloakTokenParsed {
+      roles?: string[];
+    }
+  }
 }
